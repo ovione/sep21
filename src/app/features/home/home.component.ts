f2cf36112ff2f90dfe06822a7732412e42446401
lairase <Ovidio.LAIRANA-FRANCO@ext.ec.europa.eu>
2026-07-07 14:06:53 +0200

    sep migration to eui 21

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,19 +1,49 @@
 import {Component, inject} from '@angular/core';
-import {CONFIG_TOKEN, EuiAppConfig} from '@eui/core';
-import {EUI_PAGE} from '@eui/components/eui-page';
 import {TranslateModule} from '@ngx-translate/core';
+import {
+    LayoutCoordinatorLeftTreeCenterCarouselComponent
+} from "../../core/layouts/coordinators/layout-coordinator-left-tree-center-carousel/layout-coordinator-left-tree-center-carousel.component";
+import {
+    LayoutCoordinatorLeftTreeCenterAnyComponent
+} from "../../core/layouts/coordinators/layout-coordinator-left-tree-center-any/layout-coordinator-left-tree-center-any.component";
+import {
+    LayoutContentLeftTreeEuiTreeComponent
+} from "../../core/layouts/content/layout-content-left-tree-eui-tree/layout-content-left-tree-eui-tree.component";
+import {CarouselAsCardComponent} from "../../core/components/carousel/carousel-as-card/carousel-as-card.component";
+import {ObligationDeadlineComponent} from "../obligations/carousel/obligation-deadline/obligation-deadline.component";
+import {MainNoticeComponent} from "./main-notice/main-notice.component";
+import {ConfigCarouselInput} from "../../core/components/carousel/model/config-carousel-input.model";
+import {ConfigTreeInput} from "../../core/layouts/content/layout-content-left-tree/model/config-tree-input.model";
+import {
+    ConfigLeftColumn
+} from "../../core/layouts/templates/layout-template-left-center/model/config-left-column.model";
+import {LayoutConfiguratorForHomeService} from "./layout/configuration/layout-configurator-for-home.service";
 
 @Component({
     templateUrl: './home.component.html',
     imports: [
         TranslateModule,
-        ...EUI_PAGE,
+        LayoutCoordinatorLeftTreeCenterCarouselComponent,
+        LayoutCoordinatorLeftTreeCenterAnyComponent,
+        LayoutContentLeftTreeEuiTreeComponent,
+        CarouselAsCardComponent,
+        ObligationDeadlineComponent,
+        MainNoticeComponent,
     ],
 })
 export class HomeComponent {
-    protected config: EuiAppConfig = inject(CONFIG_TOKEN);
+    configLeftColumnHome: ConfigLeftColumn;
+    configLeftContentHome: ConfigTreeInput;
+    configRightContentHome: ConfigCarouselInput;
+    private layoutConfiguratorForHomeService = inject(LayoutConfiguratorForHomeService);
 
-    constructor() {
-        console.log(this.config);
+    ngOnInit() {
+        this.configureNewLayout();
+    }
+
+    private configureNewLayout() {
+        this.configLeftColumnHome = this.layoutConfiguratorForHomeService.getConfigLeftColumn();
+        this.configLeftContentHome = this.layoutConfiguratorForHomeService.getConfigTreeInput();
+        this.configRightContentHome = this.layoutConfiguratorForHomeService.getConfigCarouselInput();
     }
 }
